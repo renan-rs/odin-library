dd76f8eb68992615c74aba84998bc69feef1d8cd
Renan Siqueira <renan.siqueira8a@gmail.com>
2025-01-06 12:59:15 -0300

    Add read toggle feature on book cards

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const booksContainer = document.querySelector(".books");
 const btnNewBook = document.getElementById("new-book");
 const dialog = document.getElementById("dialog-new-book");
 const dialogForm = document.getElementById("dialog-form");
 const btnCloseDialog = dialog.querySelector("#dialog-close");
 const btnAddBook = dialog.querySelector("#add-book");
-const booksContainer = document.querySelector(".books");
+const readCheckbox = dialog.querySelector("#book-read");
+const readOutput = dialog.querySelector("#read-output");
+// const actionRemove = document.querySelector(".action-remove");
+// const actionRead = document.querySelector(".action-read");
 const myLibrary = [];
 
 function Book(title, author, numOfPages, read){
@@ -16,17 +20,18 @@ function Book(title, author, numOfPages, read){
 function addBookToLibrary(title, author, numOfPages, read){
     const book = new Book(title, author, numOfPages, read);
     myLibrary.push(book);
+    showLibrary();
 }
 
 function showLibrary(){
     booksContainer.textContent = '';
-    myLibrary.forEach((el) => {
-        const book = createBookElement(el.title, el.author, el.numOfPages, el.read);
+    myLibrary.forEach((el, idx) => {
+        const book = createBookElement(el.title, el.author, el.numOfPages, el.read, idx);
         booksContainer.appendChild(book);
     })
 }
 
-function createBookElement(title, author, pages, read){
+function createBookElement(title, author, pages, read, idx){
     const book = document.createElement("div");
     book.classList.add("book");
     const bookTitle = document.createElement("h3");
@@ -44,15 +49,42 @@ function createBookElement(title, author, pages, read){
     readYesNo.classList.add("book-read");
     bookRead.appendChild(readYesNo);
 
-    book.append(bookTitle, bookAuthor, bookPages, bookRead);
+    const bookActions = document.createElement("div")
+    bookActions.classList.add("book-actions");
+    const actionRemove = document.createElement("div");
+    actionRemove.classList.add("action-remove");
+    const removeIcon = document.createElement("i");
+    removeIcon.classList.add("fa-solid", "fa-trash-can");
+    const removeLabel = document.createElement("span");
+    removeLabel.textContent = "Remove";
+    actionRemove.append(removeIcon, removeLabel);
+
+    const actionRead = document.createElement("div");
+    actionRead.classList.add("action-read");
+    const readIcon = document.createElement("i");
+    const readLabel = document.createElement("span");
+    readLabel.textContent = "Read";
+    actionRead.append(readIcon, readLabel);
+
+    bookActions.append(actionRemove, actionRead);
+
+    book.append(bookTitle, bookAuthor, bookPages, bookRead, bookActions);
 
     bookTitle.textContent = title;
     authorName.textContent += author;
     pagesNumber.textContent += pages;
-    readYesNo.textContent += read ? "Yes" : "No"
-    if(read){
+    readYesNo.textContent += read ? "Yes" : "No";
+    book.setAttribute("data-index", idx)
+
+    if(read) {
         readYesNo.classList.add("read");
+        readIcon.classList.add("fa-solid", "fa-toggle-on");
+    } else {
+        readIcon.classList.add("fa-solid", "fa-toggle-off");
     }
+    
+    actionRead.addEventListener("click", clickActionRead);
+    actionRemove.addEventListener("click", clickActionRemove);
     
     return book;
 }
@@ -71,12 +103,33 @@ dialogForm.addEventListener("submit", (e) => {
     const bookAuthor = dialog.querySelector("#book-author").value;
     const bookPages = parseInt(dialog.querySelector("#book-pages").value);
     const bookRead = dialog.querySelector("#book-read").checked;
-    //console.log("form values: " + `${bookTitle} - ${bookAuthor} - ${bookPages} - ${bookRead}`);
+
     addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
-    showLibrary();
+    
     dialog.close();
 });
+
+readCheckbox.addEventListener("change", (e) => {
+    if(e.target.checked){
+        readOutput.textContent = "Yes";
+    } else {
+        readOutput.textContent = "No";
+    }
+});
+
+function clickActionRead(e){
+    const dataIndex = e.currentTarget.closest(".book").getAttribute("data-index");
+    myLibrary[dataIndex].read = !myLibrary[dataIndex].read;
+    showLibrary();
+}
+function clickActionRemove(e){
+    const dataIndex = e.currentTarget.closest(".book").getAttribute("data-index");
+    //myLibrary[dataIndex].read = !myLibrary[dataIndex].read;
+    //showLibrary();
+    console.log(dataIndex);
+}
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary('Harry Potter', 'J.K. Rowling', 236, true);
 showLibrary();
+
